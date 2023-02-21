ec1d6d9bc562e3a56ffd2b97c4c7ca4bc04d0ae4
Ömer Faruk KONCA <190541018@firat.edu.tr>
2023-02-21 13:18:02 +0300

    Update main.cpp

--- newimgui-master/examples/example_glfw_opengl3/main.cpp
+++ newimgui-master/examples/example_glfw_opengl3/main.cpp
@@ -161,12 +161,12 @@ int main(int, char**)
             ImVector<ImVec2> Data;
             RollingBuffer() {
                 Span = 10.0f;
-                Data.reserve(2000);
+                Data.reserve(2000); 
             }
             void AddPoint(float x, float y) {
                 float xmod = fmodf(x, Span);
                 if (!Data.empty() && xmod < Data.back().x)
-                    Data.shrink(0);
+                    Data.shrink(0); 
                 Data.push_back(ImVec2(xmod, y));
             }
         };
