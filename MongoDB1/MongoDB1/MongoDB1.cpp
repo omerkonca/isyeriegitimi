ecf4c6a13bb6eacb15d8ce0a2793c378a9c6d262
Ömer Faruk KONCA <190541018@firat.edu.tr>
2023-02-24 10:26:57 +0300

    update

--- MongoDB1/MongoDB1/MongoDB1.cpp
+++ MongoDB1/MongoDB1/MongoDB1.cpp
@@ -1,48 +1,111 @@
-﻿#include <mongocxx/client.hpp>
-#include <bsoncxx/builder/stream/document.hpp>
+﻿#include <thread>
+
+
 #include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/stdx.hpp>
 #include <mongocxx/uri.hpp>
 #include <mongocxx/instance.hpp>
-#include <algorithm>
-#include <iostream>
-#include <vector>
-using namespace std;
-std::string getEnvironmentVariable(std::string environmentVarKey)
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/array.hpp>
+#include <bsoncxx/document/value.hpp>
+#include <bsoncxx/document/view.hpp>
+#include <mongocxx/options/find.hpp>
+#include <mongocxx/collection.hpp>
+
+using bsoncxx::builder::stream::close_array;
+using bsoncxx::builder::stream::close_document;
+using bsoncxx::builder::stream::document;
+using bsoncxx::builder::stream::finalize;
+using bsoncxx::builder::stream::open_array;
+using bsoncxx::builder::stream::open_document;
+
+
+void task1(std::string msg)
 {
-	char* pBuffer = nullptr;
-	size_t size = 0;
-	auto key = environmentVarKey.c_str();
-	// Use the secure version of getenv, ie. _dupenv_s to fetch environment variable.
-	if (_dupenv_s(&pBuffer, &size, key) == 0 && pBuffer != nullptr)
-	{
-		std::string environmentVarValue(pBuffer);
-		free(pBuffer);
-		return environmentVarValue;
-	}
-	else
-	{
-		return "";
-	}
+    //std::cout << "task1 says: " << msg;
+
+    // MONGO
+   /*  mongocxx::instance inst{};
+     mongocxx::client conn{ mongocxx::uri{} };
+
+     bsoncxx::builder::stream::document document{};
+
+     auto collection = conn["testdb"]["testcollection"];
+     document << "hello" << "world";
+
+     collection.insert_one(document.view());
+     auto cursor = collection.find({});
+
+     for (auto&& doc : cursor) {
+         std::cout << bsoncxx::to_json(doc) << std::endl;
+     }*/
+
+     ///
+    mongocxx::instance inst{};
+    mongocxx::client conn{ mongocxx::uri{"mongodb://192.168.2.125:27017" } };
+
+    //  bsoncxx::builder::stream::document document{};
+
+    auto collection = conn["robutel_local_log"]["NSWLog"];
+    //document << "hello" << "world";
+
+    //collection.insert_one(document.view());
+
+    auto order = bsoncxx::builder::stream::document{} << "$natural" << -1 << bsoncxx::builder::stream::finalize;
+
+    mongocxx::options::find opts;
+    opts.sort(order.view());
+    opts.limit(1);
+
+    auto cursor = collection.find({}, opts);
+
+    for (auto&& doc : cursor) {
+        std::cout << bsoncxx::to_json(doc) << std::endl;
+
+        bsoncxx::document::element timestamp = doc["Timestamp"];
+
+        bsoncxx::document::element wwa = doc["wWA"];
+        bsoncxx::document::element rwa = doc["rWA"];
+
+        bsoncxx::document::element wspeed = doc["wSpeed"];
+        bsoncxx::document::element rspeed = doc["rSpeed"];
+
+        bsoncxx::document::element angle = doc["Angle"];
+        bsoncxx::document::element x = doc["X"];
+        bsoncxx::document::element y = doc["Y"];
+
+        bsoncxx::document::element ti = doc["ti"];
+        bsoncxx::document::element ci = doc["ci"];
+
+
+
+        // std::cout << std::endl;
+        // std::cout << "X: " << curvename.get_int32().value << std::endl;
+
+
+    }
+
+    /* bsoncxx::stdx::optional<bsoncxx::document::value> maybe_result =
+         collection.find_one(document{} << "X" << 82464 << finalize);
+
+     if (maybe_result) {
+         std::cout << bsoncxx::to_json(*maybe_result) << "\n";
+
+
+     }*/
+
 }
-auto mongoURIStr = getEnvironmentVariable("mongodb://192.168.2.125:27017");
-static const mongocxx::uri mongoURI = mongocxx::uri{ mongoURIStr };
-// Get all the databases from a given client.
-vector<string> getDatabases(mongocxx::client& client)
+
+
+
+int main(int, char**)
 {
-	return client.list_database_names();
-}
-int main()
-{
-	// Create an instance.
-	mongocxx::instance inst{};
-	mongocxx::options::client client_options;
-	auto api = mongocxx::options::server_api{ mongocxx::options::server_api::version::k_version_1 };
-	client_options.server_api_opts(api);
-	mongocxx::client conn{ mongoURI, client_options };
-	auto dbs = getDatabases(conn);
-	for (auto db : dbs)
-	{
-		cout << db << endl;
-	}
-	return 0;
+    
+
+        std::thread t1(task1, "Hello");
+    t1.join();
+
+    
 }
