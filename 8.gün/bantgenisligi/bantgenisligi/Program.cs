26d65ae01b7e0c39a0a7f61febb545ba26006435
Ömer Faruk KONCA <190541018@firat.edu.tr>
2023-03-06 10:41:34 +0300

    update

---
+++ 8.gün/bantgenisligi/bantgenisligi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.NetworkInformation;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            long wifiBandwidth = MeasureWifiBandwidth();
+            Console.WriteLine("WiFi bant genişliği: {0} bps", wifiBandwidth);
+            Thread.Sleep(1000); // 1 saniye bekle
+        }
+    }
+
+    static long MeasureWifiBandwidth()
+    {
+        long bandwidth = 0;
+        NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+        foreach (NetworkInterface networkInterface in interfaces)
+        {
+            if (networkInterface.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && networkInterface.OperationalStatus == OperationalStatus.Up)
+            {
+                IPv4InterfaceStatistics statistics = networkInterface.GetIPv4Statistics();
+                bandwidth = networkInterface.Speed;
+            }
+        }
+        return bandwidth;
+    }
+}
