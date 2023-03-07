07b3f0da444b0bdfa8a885d86090e583395ef99d
Ömer Faruk KONCA <190541018@firat.edu.tr>
2023-03-07 15:15:23 +0300

    update

---
+++ 12.gün/zeromq_library/zeromq_library/zeromq_library.cpp
@@ -0,0 +1,18 @@
+﻿#include <zmq.hpp>
+#include <iostream>
+
+int main() {
+    zmq::context_t context(1);
+    zmq::socket_t socket(context, ZMQ_REQ);
+    socket.connect("tcp://localhost:5555");
+
+    zmq::message_t request(5);
+    memcpy(request.data(), "Merhaba", 5);
+    socket.send(request);
+
+    zmq::message_t response;
+    socket.recv(&response);
+    std::cout << "Gelen Yanıt: " << std::string(static_cast<char*>(response.data()), response.size()) << std::endl;
+
+    return 0;
+}
