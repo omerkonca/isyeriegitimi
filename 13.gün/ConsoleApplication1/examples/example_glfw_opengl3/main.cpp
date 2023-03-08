f36ef640cd1413ea1c51ee9f3111e403ee85c539
Ömer Faruk KONCA <190541018@firat.edu.tr>
2023-03-08 17:28:04 +0300

    update

--- 13.gün/ConsoleApplication1/examples/example_glfw_opengl3/main.cpp
+++ 13.gün/ConsoleApplication1/examples/example_glfw_opengl3/main.cpp
@@ -6,10 +6,6 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-
-#include "implot/implot.h"
-#include "implot/implot_internal.h"
-
 #include <stdio.h>
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
@@ -28,123 +24,25 @@
 #ifdef __EMSCRIPTEN__
 #include "../libs/emscripten/emscripten_mainloop_stub.h"
 #endif
-#include <span>
-#include <random>
+#include <string>
 #include <iostream>
+std::string ip_address;
+
 
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
-struct ScrollingBuffer {
-    int MaxSize;
-    int Offset;
-    ImVector<ImVec2> Data;
-    ScrollingBuffer(int max_size = 2000) {
-        MaxSize = max_size;
-        Offset = 0;
-        Data.reserve(MaxSize);
-    }
-    void AddPoint(float x, float y) {
-        if (Data.size() < MaxSize)
-            Data.push_back(ImVec2(x, y));
-        else {
-            Data[Offset] = ImVec2(x, y);
-            Offset = (Offset + 1) % MaxSize;
-        }
-    }
-    void Erase() {
-        if (Data.size() > 0) {
-            Data.shrink(0);
-            Offset = 0;
-        }
-    }
-};
 
-struct RollingBuffer {
-    float Span;
-    ImVector<ImVec2> Data;
-    RollingBuffer() {
-        Span = 10.0f;
-        Data.reserve(2000);
-    }
-    void AddPoint(float x, float y) {
-        float xmod = fmodf(x, Span);
-        if (!Data.empty() && xmod < Data.back().x)
-            Data.shrink(0);
-        Data.push_back(ImVec2(xmod, y));
-    }
-};
 // Main code
 int main(int, char**)
 {
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
         return 1;
-    // GLFW'nin baþlatýlmasý
-    glfwInit();
 
-    // Pencere oluþturma
-    //GLFWwindow* window = glfwCreateWindow(800, 600, "Login", NULL, NULL);
-  //  glfwMakeContextCurrent(window);
 
-    // ImGUI'nin baþlatýlmasý
-    IMGUI_CHECKVERSION();
-   
-    //ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //ImGui_ImplGlfw_InitForOpenGL(window, true);
-    //ImGui_ImplOpenGL3_Init("#version 330");
 
-    // Pencere döngüsü
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
-
-    
-
-        // Login sayfasý arayüzü
-        ImGui::SetNextWindowSize(ImVec2(400, 150), ImGuiCond_Always);
-        ImGui::SetNextWindowPos(ImVec2(200, 200), ImGuiCond_Always);
-        ImGui::Begin("Login");
-
-        static char username[128];
-        static char password[128];
-
-        ImGui::InputText("Username", username, 128);
-        ImGui::InputText("Password", password, 128, ImGuiInputTextFlags_Password);
-        if (ImGui::Button("Login"))
-        {
-            // Kullanýcý adý ve þifrenin doðruluðu kontrol edilir
-            if (std::string(username) == "admin" && std::string(password) == "password")
-            {
-                std::cout << "Login successful!" << std::endl;
-                // burada baþka bir sayfaya yönlendirebilirsiniz
-            }
-            else
-            {
-                std::cout << "Incorrect username or password. Please try again." << std::endl;
-            }
-        }
-
-        ImGui::End();
-
-        // ImGUI çizimleri yapýlýr
-        ImGui::Render();
-        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(window);
-    }
-
-    // ImGUI ve GLFW'nin sonlandýrýlmasý
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-   
-    glfwTerminate();
-
-    return 0;
-    
     // Decide GL+GLSL versions
 #if defined(IMGUI_IMPL_OPENGL_ES2)
     // GL ES 2.0 + GLSL 100
@@ -178,7 +76,6 @@ int main(int, char**)
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImPlot::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
@@ -235,75 +132,25 @@ int main(int, char**)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-     
-        //
-        //
-        //
-        //static ScrollingBuffer sdata1, sdata2;
-        //static RollingBuffer rdata1, rdata2;
-        //static float t = 0;
-        ///*t += ImGui::GetIO().DeltaTime;
-        //sdata1.AddPoint(t, 2355 * 0.0005f);
-        //sdata2.AddPoint(t, 5555 * 0.0005f);
-
-        //rdata1.AddPoint(t, 2355 * 0.0005f);
-        //rdata2.AddPoint(t, 5555 * 0.0005f);*/
-
-
-        //std::random_device rd;
-        //std::mt19937 gen(rd());
-        //std::uniform_real_distribution<> dis(2355 * 0.0005, 5555 * 0.0005);
-
-        //for (int i = 0; i < 10; ++i) {
-        //    t += ImGui::GetIO().DeltaTime;
-        //    sdata1.AddPoint(t, dis(gen));
-        //    sdata2.AddPoint(t, dis(gen));
-        //}
-
-        //for (int i = 0; i < 20; ++i) {
-        //    t += ImGui::GetIO().DeltaTime;
-        //    rdata1.AddPoint(t, dis(gen));
-        //    rdata2.AddPoint(t, dis(gen));
-        //}
-
-        //static ImPlotAxisFlags flags = ImPlotAxisFlags_NoTickLabels;
-
-
-        //static float history = 10.0f;
-
-        //ImGui::SliderFloat("History", &history, 1, 30, "%.1f s");
-        //rdata1.Span = history;
-        //rdata2.Span = history;
-
-
-        //if (ImPlot::BeginPlot("##Scrolling", ImVec2(-1, 150))) {
-        //    ImPlot::SetupAxes(NULL, NULL, flags, flags);
-        //    ImPlot::SetupAxisLimits(ImAxis_X1, t - history, t, ImGuiCond_Always);
-        //    ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 1);
-        //    ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL, 0.5f);
-        //    ImPlot::PlotShaded("Mouse X", &sdata1.Data[0].x, &sdata1.Data[0].y, sdata1.Data.size(), -INFINITY, 0, sdata1.Offset, 2 * sizeof(float));
-        //    ImPlot::PlotLine("Mouse Y", &sdata2.Data[0].x, &sdata2.Data[0].y, sdata2.Data.size(), 0, sdata2.Offset, 2 * sizeof(float));
-        //    ImPlot::EndPlot();
-        //}
-        //
+   
+        void OpenNewPage(const char* page_name) {
+            std::cout << "Opening page " << page_name << " with IP address: " << ip_address << std::endl;
+            // add logic to open new page here
+        }
        
-        //if (ImPlot::BeginPlot("##Rolling", ImVec2(-1, 150))) {
-        //    ImPlot::SetupAxes(NULL, NULL, flags, flags);
-        //    ImPlot::SetupAxisLimits(ImAxis_X1, 0, history, ImGuiCond_Always);
-        //    ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 1);
-        //    ImPlot::PlotLine("Mouse X", &rdata1.Data[0].x, &rdata1.Data[0].y, rdata1.Data.size(), 0, 0, 2 * sizeof(float));
-        //    ImPlot::PlotLine("Mouse Y", &rdata2.Data[0].x, &rdata2.Data[0].y, rdata2.Data.size(), 0, 0, 2 * sizeof(float));
-        //    ImPlot::EndPlot();
-        //}
-        //-----------------------------------------------------------------------------
+      
+        char ip_buf[13];
+        ImGui::InputText("IP address", ip_buf, sizeof(ip_buf));
+        ip_address = std::string(ip_buf); // char array'inden string'e dönüþtürme
 
-
-
-
-
-       // ImPlot::ShowDemoWindow();
-        ImGui::ShowDemoWindow();
-
+            if (ImGui::Button("Login"))
+            {
+               
+            }
+    
+            
+      
+     
         // Rendering
         ImGui::Render();
         int display_w, display_h;
@@ -322,7 +169,6 @@ int main(int, char**)
     // Cleanup
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
-    ImPlot::DestroyContext();
     ImGui::DestroyContext();
 
     glfwDestroyWindow(window);
--- 13.gün/ConsoleApplication1/imgui_demo.cpp
+++ 13.gün/ConsoleApplication1/imgui_demo.cpp
@@ -1,4 +1,82 @@
+// dear imgui, v1.89.4 WIP
+// (demo code)
 
+// Help:
+// - Read FAQ at http://dearimgui.org/faq
+// - Newcomers, read 'Programmer guide' in imgui.cpp for notes on how to setup Dear ImGui in your codebase.
+// - Call and read ImGui::ShowDemoWindow() in imgui_demo.cpp. All applications in examples/ are doing that.
+// Read imgui.cpp for more details, documentation and comments.
+// Get the latest version at https://github.com/ocornut/imgui
+
+// -------------------------------------------------
+// PLEASE DO NOT REMOVE THIS FILE FROM YOUR PROJECT!
+// -------------------------------------------------
+// Message to the person tempted to delete this file when integrating Dear ImGui into their codebase:
+// Think again! It is the most useful reference code that you and other coders will want to refer to and call.
+// Have the ImGui::ShowDemoWindow() function wired in an always-available debug menu of your game/app!
+// Also include Metrics! ItemPicker! DebugLog! and other debug features.
+// Removing this file from your project is hindering access to documentation for everyone in your team,
+// likely leading you to poorer usage of the library.
+// Everything in this file will be stripped out by the linker if you don't call ImGui::ShowDemoWindow().
+// If you want to link core Dear ImGui in your shipped builds but want a thorough guarantee that the demo will not be
+// linked, you can setup your imconfig.h with #define IMGUI_DISABLE_DEMO_WINDOWS and those functions will be empty.
+// In another situation, whenever you have Dear ImGui available you probably want this to be available for reference.
+// Thank you,
+// -Your beloved friend, imgui_demo.cpp (which you won't delete)
+
+// Message to beginner C/C++ programmers about the meaning of the 'static' keyword:
+// In this demo code, we frequently use 'static' variables inside functions. A static variable persists across calls,
+// so it is essentially like a global variable but declared inside the scope of the function. We do this as a way to
+// gather code and data in the same place, to make the demo source code faster to read, faster to write, and smaller
+// in size. It also happens to be a convenient way of storing simple UI related information as long as your function
+// doesn't need to be reentrant or used in multiple threads. This might be a pattern you will want to use in your code,
+// but most of the real data you would be editing is likely going to be stored outside your functions.
+
+// The Demo code in this file is designed to be easy to copy-and-paste into your application!
+// Because of this:
+// - We never omit the ImGui:: prefix when calling functions, even though most code here is in the same namespace.
+// - We try to declare static variables in the local scope, as close as possible to the code using them.
+// - We never use any of the helpers/facilities used internally by Dear ImGui, unless available in the public API.
+// - We never use maths operators on ImVec2/ImVec4. For our other sources files we use them, and they are provided
+//   by imgui.h using the IMGUI_DEFINE_MATH_OPERATORS define. For your own sources file they are optional
+//   and require you either enable those, either provide your own via IM_VEC2_CLASS_EXTRA in imconfig.h.
+//   Because we can't assume anything about your support of maths operators, we cannot use them in imgui_demo.cpp.
+
+// Navigating this file:
+// - In Visual Studio IDE: CTRL+comma ("Edit.GoToAll") can follow symbols in comments, whereas CTRL+F12 ("Edit.GoToImplementation") cannot.
+// - With Visual Assist installed: ALT+G ("VAssistX.GoToImplementation") can also follow symbols in comments.
+
+/*
+
+Index of this file:
+
+// [SECTION] Forward Declarations
+// [SECTION] Helpers
+// [SECTION] Demo Window / ShowDemoWindow()
+// - ShowDemoWindow()
+// - sub section: ShowDemoWindowWidgets()
+// - sub section: ShowDemoWindowLayout()
+// - sub section: ShowDemoWindowPopups()
+// - sub section: ShowDemoWindowTables()
+// - sub section: ShowDemoWindowInputs()
+// [SECTION] About Window / ShowAboutWindow()
+// [SECTION] Style Editor / ShowStyleEditor()
+// [SECTION] User Guide / ShowUserGuide()
+// [SECTION] Example App: Main Menu Bar / ShowExampleAppMainMenuBar()
+// [SECTION] Example App: Debug Console / ShowExampleAppConsole()
+// [SECTION] Example App: Debug Log / ShowExampleAppLog()
+// [SECTION] Example App: Simple Layout / ShowExampleAppLayout()
+// [SECTION] Example App: Property Editor / ShowExampleAppPropertyEditor()
+// [SECTION] Example App: Long Text / ShowExampleAppLongText()
+// [SECTION] Example App: Auto Resize / ShowExampleAppAutoResize()
+// [SECTION] Example App: Constrained Resize / ShowExampleAppConstrainedResize()
+// [SECTION] Example App: Simple overlay / ShowExampleAppSimpleOverlay()
+// [SECTION] Example App: Fullscreen window / ShowExampleAppFullscreen()
+// [SECTION] Example App: Manipulating window titles / ShowExampleAppWindowTitles()
+// [SECTION] Example App: Custom Rendering using ImDrawList API / ShowExampleAppCustomRendering()
+// [SECTION] Example App: Documents Handling / ShowExampleAppDocuments()
+
+*/
 
 #if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
 #define _CRT_SECURE_NO_WARNINGS
