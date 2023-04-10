cbcbdb7629823ff96e82b6c13f2211f75a8b6486
Ömer Faruk KONCA <190541018@firat.edu.tr>
2023-04-10 17:43:01 +0300

    update

--- 35.gün/mongodbdeneme/mongodbdeneme/Program.cs
+++ 35.gün/mongodbdeneme/mongodbdeneme/Program.cs
@@ -1,32 +1,42 @@
-﻿using MongoDB.Driver;
+﻿
+
+
+using MongoDB.Driver;
 using MongoDB.Bson;
-using MongoDB.Bson.IO;
-
-
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
         var client = new MongoClient("mongodb://192.168.2.107:27017");
-        var database = client.GetDatabase("Master_1071");
-        var collection = database.GetCollection<BsonDocument>("GeneralLog");
+        var database = client.GetDatabase("robutel_local_log");
+        var collection = database.GetCollection<BsonDocument>("CurrentSpeedLog");
 
         var startDate = new DateTime(2023, 4, 1);
-        var endDate = new DateTime(2023, 4, 5);
+        var endDate = new DateTime(2023, 4, 10);
 
         var filter = Builders<BsonDocument>.Filter.And(
             Builders<BsonDocument>.Filter.Gte("Timestamp", startDate),
             Builders<BsonDocument>.Filter.Lte("Timestamp", endDate)
         );
 
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
         var documents = collection.Find(filter).ToList();
 
-        foreach (var document in documents)
-        {
-            var json = document.ToJson(new JsonWriterSettings { Indent = true }); // BSON belgesini JSON formatına dönüştürülüyor
-            Console.WriteLine(json);
-        }
+        stopwatch.Stop();
+
+        //foreach (var document in documents)
+        //{
+        //    var json = document.ToJson(new JsonWriterSettings { Indent = true }); // BSON belgesini JSON formatına dönüştürülüyor
+        //    Console.WriteLine(json);
+        //}
+
+        Console.WriteLine($"{documents.Count} adet belge bulundu.");
+        Console.WriteLine($"Sorgu {stopwatch.Elapsed.TotalSeconds} saniyede tamamlandı.");
+
         Console.ReadLine();
     }
 }
