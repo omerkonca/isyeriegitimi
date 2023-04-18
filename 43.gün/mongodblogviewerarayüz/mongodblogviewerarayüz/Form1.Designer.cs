777e6a166b7ed2ee899a7da8b95e2f6f91254e65
Ömer Faruk KONCA <190541018@firat.edu.tr>
2023-04-18 10:08:58 +0300

    update

--- 43.gün/mongodblogviewerarayüz/mongodblogviewerarayüz/Form1.Designer.cs
+++ 43.gün/mongodblogviewerarayüz/mongodblogviewerarayüz/Form1.Designer.cs
@@ -32,13 +32,19 @@
             button1 = new Button();
             button2 = new Button();
             label1 = new Label();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // richTextBox1
             // 
+            richTextBox1.BackColor = SystemColors.InactiveCaptionText;
+            richTextBox1.ForeColor = SystemColors.InactiveBorder;
             richTextBox1.Location = new Point(322, 114);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(425, 206);
+            richTextBox1.Size = new Size(915, 450);
             richTextBox1.TabIndex = 0;
             richTextBox1.Text = "";
             // 
@@ -48,7 +54,7 @@
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 1;
-            button1.Text = "button1";
+            button1.Text = "İleri Sayfa";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
@@ -58,24 +64,63 @@
             button2.Name = "button2";
             button2.Size = new Size(94, 29);
             button2.TabIndex = 2;
-            button2.Text = "button2";
+            button2.Text = "Geri Sayfa";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(392, 60);
+            label1.Location = new Point(198, 63);
             label1.Name = "label1";
             label1.Size = new Size(50, 20);
             label1.TabIndex = 3;
             label1.Text = "label1";
             // 
+            // button3
+            // 
+            button3.Location = new Point(831, 54);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 4;
+            button3.Text = "git";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(951, 55);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(425, 25);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 7;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(425, 80);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 8;
+            label3.Text = "label3";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(897, 516);
+            ClientSize = new Size(1249, 599);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
             Controls.Add(label1);
             Controls.Add(button2);
             Controls.Add(button1);
@@ -93,5 +138,9 @@
         private Button button1;
         private Button button2;
         private Label label1;
+        private Button button3;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
     }
 }
--- 43.gün/mongodblogviewerarayüz/mongodblogviewerarayüz/Form1.cs
+++ 43.gün/mongodblogviewerarayüz/mongodblogviewerarayüz/Form1.cs
@@ -8,6 +8,8 @@ namespace mongodblogviewerarayüz
     {
         private int _pageNumber = 1;
         private int _pageSize = 50;
+        private int _totalPages = 1;
+        private long _totalDocuments = 0;
         public Form1()
         {
             InitializeComponent();
@@ -29,16 +31,47 @@ namespace mongodblogviewerarayüz
 
         private async void button2_Click(object sender, EventArgs e)
         {
-            _pageNumber++;
-            await DisplayData();
+            if (_pageNumber < _totalPages)
+            {
+                _pageNumber++;
+                await DisplayData();
+            }
         }
+
+
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int pageNumber))
+            {
+                if (pageNumber > 0 && pageNumber <= _totalPages)
+                {
+                    _pageNumber = pageNumber;
+                    await DisplayData();
+                }
+                else
+                {
+                    MessageBox.Show($"Geçerli bir sayfa numarası giriniz (1 - {_totalPages})");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Geçerli bir sayfa numarası giriniz");
+            }
+        }
+
+
         private async System.Threading.Tasks.Task DisplayData()
         {
             var connectionString = "mongodb://192.168.2.125:27017";
             var client = new MongoClient(connectionString);
             var database = client.GetDatabase("robutel_local_log");
-            var collection = database.GetCollection<BsonDocument>("ReportLog");
+            var collection = database.GetCollection<BsonDocument>("GeneralLog");
             var filter = Builders<BsonDocument>.Filter.Empty;
+
+            _totalDocuments = await collection.CountDocumentsAsync(filter);
+            _totalPages = (int)Math.Ceiling((double)_totalDocuments / _pageSize);
+
             var documents = await collection.Find(filter).Skip((_pageNumber - 1) * _pageSize).Limit(_pageSize).ToListAsync();
 
             richTextBox1.Clear();
@@ -57,10 +90,13 @@ namespace mongodblogviewerarayüz
                 richTextBox1.AppendText("\n");
             }
 
-            label1.Text = "Sayfa " + _pageNumber;
+            label1.Text = $"Sayfa {_pageNumber} / {_totalPages}";
+            label2.Text = $"Toplam belge sayısı: {_totalDocuments}";
+            label3.Text = $"Sayfa başına belge sayısı: {_pageSize}";
+            textBox1.Text = _pageNumber.ToString();
 
             button1.Enabled = (_pageNumber > 1);
-            button2.Enabled = (documents.Count == _pageSize);
+            button2.Enabled = (_pageNumber < _totalPages);
         }
     }
 }
