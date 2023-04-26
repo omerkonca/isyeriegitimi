4acc856bca80f58f5696b906a14d57445d344cdc
Ömer Faruk KONCA <190541018@firat.edu.tr>
2023-04-26 17:48:02 +0300

    update

--- 50.gün/newindexmongodb/newindexmongodb/Program.cs
+++ 50.gün/newindexmongodb/newindexmongodb/Program.cs
@@ -1,43 +1,17 @@
-﻿using Quartz;
-using Quartz.Impl;
+﻿using MongoDB.Bson;
 using MongoDB.Driver;
 
-// Quartz.NET zamanlayıcıyı oluşturun
-var schedulerFactory = new StdSchedulerFactory();
-var scheduler = await schedulerFactory.GetScheduler();
+var client = new MongoClient("mongodb://192.168.1.107:27017");
+var database = client.GetDatabase("robutel_local_log");
+var collection = database.GetCollection<BsonDocument>("NavigationLog");
 
-// Indeks oluşturmak için görev oluşturun
-var indexJob = JobBuilder.Create<CreateIndexJob>()
-    .Build();
 
-// Görevi belirli saatlerde çalıştırmak için tetikleyici (trigger) oluşturun
-var indexTrigger = TriggerBuilder.Create()
-    .WithDailyTimeIntervalSchedule(x => x
-        .OnMondayThroughFriday()
-        .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(8, 0)) // 8:00'da başla
-        .EndingDailyAt(TimeOfDay.HourAndMinuteOfDay(17, 0)) // 17:00'da bitir
-        .WithIntervalInSeconds(60)) // Her dakika çalıştır
-    .Build();
+Console.WriteLine((DateTime.Now.ToString("HH:m:s:fff")));
+var indexModel = new CreateIndexModel<BsonDocument>(Builders<BsonDocument>.IndexKeys.Ascending("Timestamp"));
+Console.WriteLine("--------");
 
-// Indeks oluşturma görevini zamanlayıcıya ekleyin
-await scheduler.ScheduleJob(indexJob, indexTrigger);
 
-// Quartz.NET zamanlayıcıyı başlatın
-await scheduler.Start();
+await collection.Indexes.CreateOneAsync(indexModel);
 
-// Indeks oluşturma görevi
-public class CreateIndexJob : IJob
-{
-    public async Task Execute(IJobExecutionContext context)
-    {
-        var client = new MongoClient("mongodb://192.168.1.107:27017"); // MongoDB bağlantısını oluşturun
-        var database = client.GetDatabase("robutel_local_log"); // Veritabanını seçin
-        var collection = database.GetCollection<MyClass>("CurrentSpeedLog"); // Koleksiyonu seçin
 
-        var keys = Builders<MyClass>.IndexKeys.Ascending(x => x.id); // Indeks anahtarlarını belirtin
-
-        var options = new CreateIndexOptions { Unique = true }; // Indeks seçeneklerini belirtin
-
-        await collection.Indexes.CreateOneAsync(new CreateIndexModel<MyClass>(keys, options)); // Indeks oluştur
-    }
-}
+Console.WriteLine((DateTime.Now.ToString("HH:m:s:fff")));
