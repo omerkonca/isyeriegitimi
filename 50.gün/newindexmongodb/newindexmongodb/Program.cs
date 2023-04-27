aa1ba39632cb0f65e362113ee196441a100c3e15
Ömer Faruk KONCA <190541018@firat.edu.tr>
2023-04-27 14:36:54 +0300

    update

--- 50.gün/newindexmongodb/newindexmongodb/Program.cs
+++ 50.gün/newindexmongodb/newindexmongodb/Program.cs
@@ -3,7 +3,7 @@ using MongoDB.Driver;
 
 var client = new MongoClient("mongodb://192.168.1.107:27017");
 var database = client.GetDatabase("robutel_local_log");
-var collection = database.GetCollection<BsonDocument>("NavigationLog");
+var collection = database.GetCollection<BsonDocument>("BatteryLog");
 
 
 Console.WriteLine((DateTime.Now.ToString("HH:m:s:fff")));
